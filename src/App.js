5ac860d21b2bd409d6f2bdeb6f4b9c6c845df393
akhilprasadap <48425813+akhilprasadap@users.noreply.github.com>
2019-03-27 17:25:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home.js';
+//import Profile from './Profile.js';
+import ReactDOM from 'react-dom';
+//import Add from './Add.js';
+import {BrowserRouter as Router, Link,NavLink,Redirect} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+              <div className="App">                
+                 
+                  <Route path = "/" exact component = {Home} />        
+                  
+              </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
